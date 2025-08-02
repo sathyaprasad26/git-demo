d5c8daa89df561266a547a80055e6f058ca111b7
sathyaprasad26 <acharyasathyaprasad@gmail.com>
2025-08-03 00:09:57 +0530

    Added new file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        float num = 5.85f;
+        String str1 ="value";
+        String str2 = new String("values");
+        String str3 = String.valueOf("123");         
+        System.out.println("Hello, World!");
+        System.out.println("The number is: " + num);
+    System.out.println("The string is: " + str1 +str2 + str3);
+    }
+}
